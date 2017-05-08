3b5dac165da27f8e865e11a2862490d1398d791b
MiltonLn <miltonln04@gmail.com>
2017-05-08 00:25:54 -0500

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,41 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+
+// Routes
+import { APP_ROUTING } from './app.routes';
+
+// Services
+import { HeroesService } from './services/heroes.service';
+
+// Components
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroeComponent } from './components/heroe/heroe.component';
+import { BuscadorHeroesComponent } from './components/buscador-heroes/buscador-heroes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    HeroesComponent,
+    HeroeComponent,
+    BuscadorHeroesComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    APP_ROUTING
   ],
-  providers: [],
+  providers: [
+    HeroesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
